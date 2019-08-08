b0171e6945ed18dc60dbd3c126af92d81d4597a8
Darren Parkinson <dparkinson@natilik.com>
2019-08-08 10:37:27 +0100

    Initial version

---
+++ features/webex_cards.js
@@ -0,0 +1,46 @@
+const samplecards = require('../libs/sample_cards');
+
+module.exports = function (controller) {
+
+    controller.hears('help', 'message,direct_message', async (bot, message) => {
+        console.log(samples.join("  \n* "))
+        await bot.reply(message, { markdown: helpmarkdown });
+    });
+
+    controller.hears(async (message) => message.text && samples.includes(message.text.toLowerCase()), ['message', 'direct_message'], async (bot, message) => {
+
+        await bot.reply(message, {
+            text: "cards not supported on this platform yet",
+            attachments: samplecards[message.text.toLowerCase()]
+        });
+    });
+
+
+    const samples = [
+        "activityupdate",
+        "agenda",
+        "calendarreminder",
+        "expensereport",
+        "flightdetails",
+        "flightitinerary",
+        "flightupdate",
+        "foodorder",
+        "imagegallery",
+        "inputform",
+        "inputs",
+        "productvideo",
+        "restaurant",
+        "sportingevent",
+        "stockupdate",
+        "weathercompact",
+        "weatherlarge",
+    ]
+
+    const helpmarkdown = `
+## Samples 
+
+Use the following commands to see the samples from [adaptivecards.io](https://adaptivecards.io/samples/):  
+
+* ${samples.join("  \n* ")}
+`
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Botkit Cards
+
+A simple application for testing the [adaptive cards samples](https://adaptivecards.io/samples/) with Webex Teams. 
+
+This project is based on the Botkit starter kit for Webex and was created with the [Yeoman generator](https://github.com/howdyai/botkit/tree/master/packages/generator-botkit#readme).
+
+To complete the configuration of this bot, make sure to update the included `.env` file with your platform tokens and credentials.  See the `.env.sample` file for an example.  Also refer to the [Botkit Docs](https://botkit.ai/docs/v4).
